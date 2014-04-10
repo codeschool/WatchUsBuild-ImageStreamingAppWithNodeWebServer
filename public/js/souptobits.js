730aa5962c99987cae3d03142220d4339ca73f87
TJ Krusinski <tjkrus@gmail.com>
2014-04-10 16:34:21 -0400

    update to handle badge as object

--- public/js/souptobits.js
+++ public/js/souptobits.js
@@ -4,6 +4,6 @@ $(function(){
   var socket = io.connect();
 
   socket.on('badge', function(badge){
-    $('html').prepend('<img src="'+badge+'">');
+    $('html').prepend('<img src="https://d1ffx7ull4987f.cloudfront.net/images/courses/large_badge/'+badge.course_id+'/'+badge.badge_id+'">');
   });
 });

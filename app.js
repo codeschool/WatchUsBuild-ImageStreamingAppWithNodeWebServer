8186fa9b9db3ccab42e3382c274b9f84fb81f6a0
TJ Krusinski <tjkrus@gmail.com>
2014-04-11 14:13:03 -0400

    get the most recent 10 badges

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 var sub = require('./lib/sub');
+var badges = require('./lib/badges');
 
 /**
  *  Have our server listen on port 3000
@@ -28,7 +29,13 @@ app.get('/', function(req, res){
 /**
  *  Watch for connections
  */
-io.sockets.on('connection', function(socket){ });
+io.sockets.on('connection', function(socket){
+  badges.get(function(err, badgeList){
+    badgeList.forEach(function(badge){
+      socket.emit('badge', badge);
+    });
+  });
+});
 
 /**
  *  When a message comes in from the pub/sub system, send it to the sockets

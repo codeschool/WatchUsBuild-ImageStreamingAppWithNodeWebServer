1c27139f0776668fed74203d9b7a4c2d9748d061
TJ Krusinski <tjkrus@gmail.com>
2014-04-13 10:29:58 -0400

    animate on badges

--- public/js/souptobits.js
+++ public/js/souptobits.js
@@ -2,8 +2,13 @@
 
 $(function(){
   var socket = io.connect();
+  var src ='<img src="https://d1ffx7ull4987f.cloudfront.net/images/courses/large_badge/';
 
   socket.on('badge', function(badge){
-    $('html').prepend('<img src="https://d1ffx7ull4987f.cloudfront.net/images/courses/large_badge/'+badge.course_id+'/'+badge.badge_id+'">');
+    var img = $(src+badge.course_id+'/'+badge.badge_id+'">');
+    $('.badge-wrapper').prepend(img);
+    setTimeout(function(){
+      img.addClass('on');
+    }, 0);
   });
 });

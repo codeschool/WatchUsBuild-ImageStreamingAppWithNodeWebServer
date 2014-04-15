894a9a1df6becf9a1a8e3a246f87f5894b46c15b
TJ Krusinski <tjkrus@gmail.com>
2014-04-15 09:24:36 -0400

    support host from process.env for badges

--- models/badges.js
+++ models/badges.js
@@ -1,12 +1,13 @@
 'use strict';
 
+var host = process.env.BADGES_HOST || 'http://localhost:8000/badges';
 var request = require('request');
 
 /**
  *  Load the last 10 badges from the pub/sub server
  */
 exports.get = function(callback) {
-  request('http://localhost:8000/badges', function(err, resp, data){
+  request(host, function(err, resp, data){
     data = JSON.parse(data);
     if (data.error) return callback(err, []);
     callback(null, data.data);

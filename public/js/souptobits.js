464df722c3179ea3acf50f8e655bae5f5061453f
TJ Krusinski <tjkrus@gmail.com>
2014-04-15 13:20:00 -0400

    change badge object to only contain id

--- public/js/souptobits.js
+++ public/js/souptobits.js
@@ -2,10 +2,9 @@
 
 $(function(){
   var socket = io.connect();
-  var src ='<img src="https://d1ffx7ull4987f.cloudfront.net/images/courses/large_badge/';
 
   socket.on('badge', function(badge){
-    var img = $(src+badge.course_id+'/'+badge.badge_id+'">');
+    var img = $('<img src="'+badge.badge_id+'">');
     $('.badge-wrapper').prepend(img);
     setTimeout(function(){
       img.addClass('on');
